d0ade982055b66d2ca2c746585e0536c1c778601
Filip Bukowiec <filip.bukowiec@ad.ruckzuck.pl>
2025-03-24 15:49:28 +0100

    add top-menu component

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TopMenuComponent } from "../top-menu/top-menu.component";
 
 @Component({
   selector: 'app-admin',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TopMenuComponent],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from '../footer/footer.component';
+import { TopMenuComponent } from '../top-menu/top-menu.component';
 
 
 @Component({
   standalone:true,
-  imports: [CommonModule, FormsModule, FooterComponent],
+  imports: [CommonModule, FormsModule, TopMenuComponent],
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
