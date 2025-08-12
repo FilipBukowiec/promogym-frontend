eed6ebb51a3618b1a56413fa6193d1cfa5de2f24
unknown <filip.bukowiec@interia.pl>
2025-08-12 23:09:01 +0200

    add component & service sentences

---
+++ src/app/components/sentences/sentences.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sentences',
+  imports: [],
+  templateUrl: './sentences.component.html',
+  styleUrl: './sentences.component.scss'
+})
+export class SentencesComponent {
+
+}
