75018c96ea41da689fd25c60698bfbbd198b6ad9
Krzysztof Rybicki <krybicki95@gmail.com>
2025-10-28 15:48:45 +0100

    GYM-14 is kiosk

--- src/app/auth/interceptors/user-data.interceptor.ts
+++ src/app/auth/interceptors/user-data.interceptor.ts
@@ -1,14 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { iif, switchMap, take } from 'rxjs';
+import { combineLatest, iif, switchMap, take } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 export const userDataInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  return authService.isAdmin().pipe(
-    switchMap((isAdmin) => iif(() => isAdmin, authService.selectCurrentTenant().pipe(take(1)), authService.selectUserInfo())),
+  return combineLatest([authService.isAdmin(), authService.isKiosk()]).pipe(
+    switchMap(([isAdmin, isKiosk]) => iif(() => isAdmin || isKiosk, authService.selectCurrentTenant().pipe(take(1)), authService.selectUserInfo())),
     switchMap((data) => {
       if (data) {
+        console.log(req);
         const cloned = req.clone({
           setHeaders: {
             'tenant-id': data.tenant_id,
